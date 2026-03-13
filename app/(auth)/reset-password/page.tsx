2d4e55f171843169f7f79e1e2746c6fff1b6dc36
Pandit Pawar <iampandit.in@gmail.com>
2026-03-14 00:15:44 +0530

    added the suspense bourndry in the reset password form

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,6 +1,7 @@
 import ResetPasswordForm from "@/components/auth/reset-password";
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function ResetPasswordPage() {
   return (
@@ -9,7 +10,9 @@ export default function ResetPasswordPage() {
         <Image src={"/logo.png"} alt="logo" height={25} width={25} />
         <p className="text-lg font-medium">Shado/auth</p>
       </Link>
-      <ResetPasswordForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ResetPasswordForm />
+      </Suspense>
       <p className="text-muted-foreground text-sm">
         Don&apos;t have an account?{" "}
         <Link href={"/sign-up"} className="hover:underline text-primary">
